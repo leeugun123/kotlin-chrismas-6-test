b7613ce89dbe5176c11c44a497f4bf0cb90711b4
leeugun123 <leeugun123@naver.com>
2023-11-15 18:00:09 +0900

    메뉴 예외 처리 -- 잘못된 형식 예외 처리

--- src/main/kotlin/chrismas/ExceptionMangement/ExceptionHandle.kt
+++ src/main/kotlin/chrismas/ExceptionMangement/ExceptionHandle.kt
@@ -6,16 +6,26 @@ object ExceptionHandle {
     private const val INPUT_AGAIN = "다시 입력해 주세요."
     private const val WRONG_DATE = "$ERROR 유효하지 않는 날짜입니다.$INPUT_AGAIN"
     private const val INVALID_FORMAT = "$ERROR 잘못된 형식입니다.$INPUT_AGAIN"
-
+    private const val INVALID_MENU_ORDER = "$ERROR 유효하지 않은 주문입니다.$INPUT_AGAIN"
 
     fun checkDate(date : String){
         require(checkValidDate(date)){ INVALID_FORMAT}
         require(checkDateInRange(date)){ WRONG_DATE}
     }
 
+    fun checkMenuInput(menuOrder : String){
+
+        require(checkValidMenuFormat(menuOrder)) { INVALID_MENU_ORDER}
+
+    }
+
     private fun checkValidDate(date : String) = date.toIntOrNull() != null
 
     private fun checkDateInRange(date : String) = date.toInt() in 1..31
+
+    private fun checkValidMenuFormat(menu : String) : Boolean = Regex("""^[a-zA-Z가-힣]+\-\d+(,[a-zA-Z가-힣]+\-\d+)*$""").matches(menu)
+
+
 
 
 
--- src/main/kotlin/chrismas/View/InputView.kt
+++ src/main/kotlin/chrismas/View/InputView.kt
@@ -24,8 +24,22 @@ class InputView {
 
     private fun readMenu(){
         println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
-        val menuTuple = Console.readLine()
-        Parsing.menuParsing(menuTuple)
+
+        while(true){
+            try {
+                val menuTuple = Console.readLine()
+                ExceptionHandle.checkMenuInput(menuTuple)
+                Parsing.menuParsing(menuTuple)
+                return
+            }catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+
+        }
+
+
+
+
     }
 
     private fun startGuide(){
